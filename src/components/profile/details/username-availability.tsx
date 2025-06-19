0fd311713d31a264e65f33140509bd4e40353ab8
webdevtolu <tolu.adeyemo7@gmail.com>
2025-06-19 14:52:03 +0100

    add stylying for unavailable username

--- src/components/profile/details/username-availability.tsx
+++ src/components/profile/details/username-availability.tsx
@@ -9,7 +9,7 @@ export const UsernameAvailability = ({
   username,
 }: UsernameAvailabilityProps) => {
   // TODO: ~> check if username if available
-  const isUsernameAvailable = username;
+  const isUsernameAvailable = !!username;
 
   return (
     <div
@@ -17,12 +17,14 @@ export const UsernameAvailability = ({
         "flex items-center gap-2 rounded-xl py-1.5 pr-3 pl-2 text-xs font-medium",
         {
           "bg-success-50 text-success-base": isUsernameAvailable,
+          "bg-error/7 text-error": !isUsernameAvailable,
         },
       )}
     >
       <CheckCircleIcon
         className={cn("size-4", {
           "text-success-base": isUsernameAvailable,
+          "text-error": !isUsernameAvailable,
         })}
       />
       {isUsernameAvailable
