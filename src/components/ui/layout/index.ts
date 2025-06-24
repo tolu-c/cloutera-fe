15353aacd50c929be63cd39be9fa5c59fe3f60d3
Olabisi Abolarin <olabisiabolarin5@gmail.com>
2025-06-24 06:32:44 +0100

    feat: implement Sidebar component and integrate with TopBar for navigation

--- src/components/ui/layout/index.ts
+++ src/components/ui/layout/index.ts
@@ -1,3 +1,4 @@
 export { default as AuthLayoutContainer } from "./auth-layout-container";
 export { default as AuthCard } from "./auth-card";
 export { default as TopBar } from "./top-bar";
+export { default as Sidebar } from "./side-bar";
