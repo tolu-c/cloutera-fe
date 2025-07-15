f1f19926ef4630ffc20f25dad0dff8e4a995f459
webdevtolu <tolu.adeyemo7@gmail.com>
2025-07-15 06:11:23 +0100

    feat(hooks): enhance `useLocalStorage` with error handling and window check
    - Add try-catch blocks for `getItem`, `setItem`, and `removeItem` to handle potential errors.
    - Include a window existence check to ensure compatibility in server-side environments.

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,40 @@
 export const useLocalStorage = <T>(key: string) => {
   const getItem = (): T | null => {
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
+    try {
+      if (typeof window === "undefined") {
+        return null;
+      }
+
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : null;
+    } catch (error) {
+      console.error("Error getting item from localStorage:", error);
+      return null;
+    }
   };
   const setItem = (value: T) => {
-    localStorage.setItem(key, JSON.stringify(value));
+    try {
+      if (typeof window === "undefined") {
+        return null;
+      }
+
+      localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error("Error setting item in localStorage:", error);
+      return null;
+    }
   };
   const removeItem = () => {
-    localStorage.removeItem(key);
+    try {
+      if (typeof window === "undefined") {
+        return null;
+      }
+
+      localStorage.removeItem(key);
+    } catch (error) {
+      console.error("Error removing item from localStorage:", error);
+      return null;
+    }
   };
   return { getItem, setItem, removeItem };
 };
